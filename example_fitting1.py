b6df5a9c7e88dbf8c8f69f791772cfc5772a5243
Aditya.parmar2903 <Aditya.parmar2903@gmail.com>
2016-07-27 14:48:10 -0400

    Example fitting code plots stage-wise

--- example_fitting1.py
+++ example_fitting1.py
@@ -3,128 +3,118 @@ import numpy as np
 from matplotlib import pyplot as plt
 from pysb.integrate import Solver
 import scipy.optimize
-from pyDOE import *
-from scipy.stats.distributions import norm
 import numdifftools as nd
-import numdifftools.nd_algopy as nda
+import pickle
 
-
-# Simulate the model
-
-# Create time vector using np.linspace function
 num_timepoints = 101
 num_dim = 3
 t = np.linspace(0, 200, num_timepoints)
+norm = np.zeros(len(model.observables))
+sol = Solver(model, t, use_analytic_jacobian=True)
 
-# Get instance of the Solver
-sol = Solver(model, t, use_analytic_jacobian=False)
-
-# Perform the integration
 sol.run()
-
-# sol.y contains timecourses for all of the species
-# (model.species gives matched list of species)
-
-# sol.yobs contains timecourse only for the observables
-# Indexed by the name of the observable
-
-# Plot the timecourses for A, B, and C
 plt.ion()
-plt.figure()
 
-# Iterate over the observables
+colors2 = ['blue', 'cornflowerblue', 'cyan', 'darkgreen', 'lawngreen', 'darkorange',
+          'yellow', 'plum', 'red', 'magenta', 'sienna', 'black']
 colors = ['red', 'green', 'blue']
+colors1 = ['pink', 'lightgreen', 'skyblue']
 data = np.zeros((num_timepoints, len(model.observables)))
+
+plt.figure()
 for obs_ix, obs in enumerate(model.observables):
     obs_max = np.max(sol.yobs[obs.name])
-    # Plot the observable
     plt.plot(t, sol.yobs[obs.name] / obs_max, label=obs.name,
              color=colors[obs_ix])
-    # Make noisy data
-    # Get random numbers
-    rand_norm = np.random.randn(num_timepoints)
-    # Multiply by the values of the observable
-    sigma = 0.1
-    noise = rand_norm * sigma * sol.yobs[obs.name]
-    # Add the noise vector to the timecourse
-    noisy_obs = noise + sol.yobs[obs.name]
-    norm_noisy_data = noisy_obs / obs_max
+    plt.legend()
+plt.suptitle('Simulation of the model equations')
+plt.xlabel('Time (in seconds)')
+plt.ylabel('Normalized Concentration')
+
+plt.figure()
+for obs_ix, obs in enumerate(model.observables):
+    obs_max = np.max(sol.yobs[obs.name])
+    plt.plot(t, sol.yobs[obs.name] / obs_max, label=obs.name,
+             color=colors[obs_ix])
+    norm_noisy_data = np.load('data_plot_robertson-%d.npy' %(obs_ix))
     plt.plot(t, norm_noisy_data, linestyle='', marker='.',
              color=colors[obs_ix])
-    data[:, obs_ix] = noisy_obs
+    plt.legend()
+plt.suptitle('Synthetic noisy data')
+plt.xlabel('Time (in seconds)')
+plt.ylabel('Normalized Concentration')
+
+data = np.load('data_robertson.npy')
 
 p_to_fit = [p for p in model.parameters
                      if p.name in ['k1', 'k2', 'k3']]
 
-# First define the objective function
 def obj_func(x):
     lin_x = 10 ** x
-    #print x
-    # Run a simulation using these parameters
-    # Initialize the model to have the values in the parameter array
+    print x
     for p_ix, p in enumerate(p_to_fit):
         p.value = lin_x[p_ix]
-    # Run the solver
     sol.run()
-    # Calculate our error
     total_err = 0
     for obs_ix, obs in enumerate(model.observables):
         y = sol.yobs[obs.name]
-        # Calculate the square difference with the data
         total_err += np.sum((y - data[:, obs_ix])**2)
-    #print total_err
+    print total_err
     return total_err
 
-# Hang on to the original values for comparison
-nominal_values = np.array([p.value for p in p_to_fit])
-x_test = np.log10(nominal_values)
-#print "True values (in log10 space):", x_test
-#print "Nominal error:", obj_func(x_test)
-
-
-
-
-# Pick a starting point; in practice this would be random selected by
-# a sampling strategy (e.g., latin hypercube sampling) or from a prior
-# distributionmeans
-x0 = np.array([np.log10(p.value * 0.1) for p in p_to_fit])
-
-# Run the minimization algorithm!
 
 def Jacob(x):
-	#x = np.asarray(x)
-	jaco = nd.Jacobian(obj_func)(x)
-	#print jaco,'aaa'
-	return jaco[0]
+    if np.any(np.isnan(x)):
+        jaco = np.zeros(x.shape)
+        jaco[:] = np.nan
+        return jaco
+    jaco = nd.Jacobian(obj_func)(x)
+    return jaco[0]
 
-def Hessi(x):
-	#x = np.asarray(x)
-	hes = nd.Hessian(obj_func)(x)
-	#print hes
-	return hes
-	
-	
-#Jacob = nda.Jacobian(obj_func, method = 'reverse')
-#Hessi = nda.Hessian(obj_func)
-result = scipy.optimize.minimize(obj_func, x0, method='trust-ncg',jac = Jacob, hess = Hessi)
+nominal_values = np.array([p.value for p in p_to_fit])
+x_test = np.log10(nominal_values)
+print "True values (in log10 space):", x_test
+print "Nominal error:", obj_func(x_test)
+
+ini_val = np.load('initial_values.npy')
+x0 = ini_val[0]
+result = scipy.optimize.minimize(obj_func, x0, method='Nelder-Mead', options = {'disp' : True})
 
 
 plt.figure()
-# Plot the original data
 plt.plot(t, data, linestyle='', marker='.', color='k')
-# Plot BEFORE
-# Set parameter values to start position
+plt.xlabel('Time (in seconds)')
+plt.ylabel('Concentration')
+plt.suptitle('Data to fit')
+
+plt.figure()
+plt.plot(t, data, linestyle='', marker='.', color='k')
 for p_ix, p in enumerate(p_to_fit):
     p.value = 10 ** x0[p_ix]
 sol.run()
-plt.plot(t, sol.y, color='red')
-# Plot AFTER
-# Set parameter values to final position
+for obs_ix, obs in enumerate(model.observables):
+    plt.plot(t, sol.yobs[obs.name], color=colors1[obs_ix],
+    label = obs.name)
+plt.legend()
+plt.xlabel('Time (in seconds)')
+plt.ylabel('Concentration')
+plt.suptitle('Initial guess')
+
+plt.figure()
+plt.plot(t, data, linestyle='', marker='.', color='k')
+for p_ix, p in enumerate(p_to_fit):
+    p.value = 10 ** x0[p_ix]
+sol.run()
+for obs_ix, obs in enumerate(model.observables):
+    plt.plot(t, sol.yobs[obs.name], color=colors1[obs_ix])
 for p_ix, p in enumerate(p_to_fit):
     p.value = 10 ** result.x[p_ix]
 sol.run()
-plt.plot(t, sol.y, color='yellow')
-
-
-
-
+for obs_ix, obs in enumerate(model.observables):
+    plt.plot(t, sol.yobs[obs.name], color=colors[obs_ix],
+    label = obs.name)
+plt.ylim(-0.2,1)
+plt.legend()
+plt.xlabel('Time (in seconds)')
+plt.ylabel('Concentration')
+plt.suptitle('Final fitting')
